624270a7dbf60d9387afe77719ee145aa4868eee
Julian Korgol <u25_julkor_waw@technischools.com>
2021-12-07 09:24:46 +0100

    Odpowiedzi do sprawdzianu Sprawdzian #2

--- execute.sql
+++ execute.sql
@@ -1,35 +1,15 @@
 USE Sprawdzian2Hotel
 GO
 
---7.
--- SELECT * FROM Pokoje
--- WHERE Pietro = 2
--- UNION
--- SELECT * FROM Pokoje
--- WHERE Cena > 200
-
-
---9.
--- SELECT g.Imie, g.Nazwisko, p.Cena * DATEDIFF(day, g.PoczatekPobytu, g.KoniecPobytu)
--- FROM Pokoje p JOIN Goscie G on p.Id = G.PokojId
-
---10.
--- SELECT g.Imie, g.Nazwisko FROM Goscie g
--- JOIN Pokoje P on g.PokojId = P.Id
--- WHERE p.Pietro = 2
-
+--Odpowiedzi do sprawdzianu:
 --11.
--- SELECT COUNT(p.Id) AS liczbaPokoi FROM Sprzatanie s
--- JOIN Pokoje P on s.PokojId = P.Id
--- WHERE s.PokojId != p.Id
-
+SELECT count(*)
+FROM Pokoje
+LEFT OUTER JOIN Sprzatanie S on Pokoje.Id = S.PokojId
+WHERE PracowinkId IS NULL
 --12.
--- SELECT p.ID FROM Pokoje p
--- JOIN Sprzatanie S on p.Id = S.PokojId
--- JOIN Goscie G on p.Id = G.PokojId
--- WHERE S.DzienTygodnia = 'sr' AND GETDATE() > g.KoniecPobytu
-
---13.
--- SELECT DISTINCT p.ID, p.Pietro, (SELECT COUNT(*) FROM Sprzatanie WHERE PokojId = p.Id) AS LiczbaSprzatanWTygodniu FROM Pokoje p
--- JOIN Sprzatanie S on p.Id = S.PokojId
-
+SELECT *
+FROM Sprzatanie
+FULL OUTER JOIN Pokoje P on P.Id = Sprzatanie.PokojId
+FULL OUTER JOIN Goscie G on P.Id = G.PokojId
+WHERE Sprzatanie.DzienTygodnia = 'œr' AND g.PokojId IS NULL
