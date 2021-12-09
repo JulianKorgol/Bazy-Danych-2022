2a24c2e547882bd1ffdc5ff63d6ad2b522057f4d
Julian Korgol <u25_julkor_waw@technischools.com>
2021-12-09 10:37:07 +0100

    2021-12-09

--- execute.sql
+++ execute.sql
@@ -1,3 +1,12 @@
+USE master
+GO
+
+DROP DATABASE IF EXISTS Szkola
+GO
+
+CREATE DATABASE SZKOLA
+GO
+
 USE Szkola
 GO
 
@@ -5,23 +14,45 @@ GO
 -- CREATE DATABASE Szkola
 
 --2. -> Na sprawdzian
-CREATE TABLE Uczen (ID INT NOT NULL IDENTITY(1,1),
-Imie VARCHAR(30) NOT NULL CHECK(LEN(Imie) >= 3),
-DrugieImie VARCHAR(30) CHECK(LEN(DrugieImie) >= 3),
-Nazwisko VARCHAR(30) NOT NULL CHECK(LEN(Nazwisko) >= 3),
-DataUrodzenia DATE NOT NULL)
+-- CREATE TABLE Uczen (ID INT NOT NULL IDENTITY(1,1),
+-- Imie VARCHAR(30) NOT NULL CHECK(LEN(Imie) >= 3),
+-- DrugieImie VARCHAR(30) CHECK(LEN(DrugieImie) >= 3),
+-- Nazwisko VARCHAR(30) NOT NULL CHECK(LEN(Nazwisko) >= 3),
+-- DataUrodzenia DATE NOT NULL)
+
+-- OdpowiedŸ z tablicy:
+-- CREATE TABLE Uczen (
+--     Id INT NOT NULL IDENTITY UNIQUE,
+--     Imie VARCHAR(30) NOT NULL CHECK(LEN(Imie) >= 3),
+--     DrugieImie VARCHAR(30) CHECK(LEN(DrugieImie) >= 3),
+--     Nazwisko VARCHAR(30) NOT NULL CHECK(LEN(Nazwisko) >= 3),
+--     DataUrodzenia DATE NOT NULL
+-- )
 
 --3.
-ALTER TABLE Uczen
-ADD SredniaOcen NUMERIC(3,2) NOT NULL CHECK(1.0 >= SredniaOcen AND SredniaOcen >= 6.0) DEFAULT(5.0)
+-- ALTER TABLE Uczen
+-- ADD SredniaOcen NUMERIC(3,2) NOT NULL CHECK(SredniaOcen >= 1.0 AND SredniaOcen <= 6.0) DEFAULT(5.0)
 
 --4.
-ALTER Table Uczen
-ALTER COLUMN Nazwisko VARCHAR(50)
+-- ALTER Table Uczen
+-- ALTER COLUMN Nazwisko VARCHAR(50) NOT NULL
 
 --5.
 -- ALTER Table Uczen
 -- ALTER COLUMN DataUrodzenia DATE NOT NULL CHECK(DATEDIFF(YEAR, GETDATE(), DataUrodzenia) >= 16)
 
-ALTER TABLE Uczen
-ADD CONSTRAINT DataUrodzenia CHECK(DATEDIFF(YEAR, GETDATE(), DataUrodzenia) >= 16)
+-- ALTER TABLE Uczen
+-- ADD CONSTRAINT CK_DataUrodzenia CHECK(DATEDIFF(YEAR, DataUrodzenia, GETDATE()) >= 16)
+
+
+
+
+
+--Druga lekcja
+
+CREATE TABLE Adresy (
+    Id INT PRIMARY KEY,
+    Ulica VARCHAR(30) NOT NULL,
+    Miasto VARCHAR(40) NOT NULL,
+    UczenId INT NOT NULL FOREIGN KEY REFERENCES Uczen(ID)
+)
