b7f3951502b032de3ef2ce7185203033c0a52e0c
Julian Korgol <u25_julkor_waw@technischools.com>
2021-12-16 10:07:49 +0100

    2021-12-14 #2

--- execute.sql
+++ execute.sql
@@ -1,12 +1,14 @@
 -- USE master
 -- GO
---
+
+-- ZAD 1
 -- DROP DATABASE IF EXISTS Panek
 -- CREATE DATABASE Panek
---
+
 -- USE Panek
 -- GO
 --
+-- Zad 2
 -- CREATE TABLE Samochody (
 --     NumerRej VARCHAR(10) NOT NULL PRIMARY KEY,
 --     Marka VARCHAR(30) NOT NULL CHECK(LEN(Marka) >= 3),
@@ -14,18 +16,18 @@
 --     CenaZaKm MONEY NOT NULL CHECK(CenaZaKm >= 1 AND 9999.99 >= CenaZaKm),
 --     Klimatyzacja CHAR(3) NOT NULL DEFAULT('TAK') CHECK(Klimatyzacja IN ('TAK', 'NIE')),
 -- )
---
+--Zad 3
 -- ALTER TABLE Samochody
 -- ADD DataBadanTech DATE CHECK(DataBadanTech >= GETDATE())
---
+--ZAD 4
 -- ALTER TABLE Samochody
 -- ALTER COLUMN Klimatyzacja CHAR(3)
---
+--ZAD 5
+ALTER TABLE Samochody
+ADD CONSTRAINT CH_Marka CHECK(Marka IN ('FORD','TOYOTA','SEAT','BMW'))
+--ZAD 6
 -- ALTER TABLE Samochody
--- ADD CONSTRAINT CH_Marka CHECK(Marka IN ('FORD','TOYOTA','SEAT','BMW'))
---
--- ALTER TABLE Samochody
--- DROP CONSTRAINT CK__Samochody__CenaZ__38996AB5 --Uwielbiam takie nazwy :D
+-- DROP CONSTRAINT CK__Samochody__CenaZ__267ABA7A --Uwielbiam takie nazwy :D
 --
 
 --ZAD 7
@@ -33,8 +35,8 @@ CREATE TABLE Klienci (
     ID INT NOT NULL IDENTITY PRIMARY KEY,
     Imie VARCHAR(30) NOT NULL,
     Nazwisko VARCHAR(30) NOT NULL,
-    KartaKredytowa VARCHAR(20) NOT NULL,
+    KartaKredytowa VARCHAR(25) NOT NULL,
     NumerRej VARCHAR(10) NOT NULL FOREIGN KEY REFERENCES Samochody(NumerRej),
-    DataWypozyczenia DATE NOT NULL,
-    DataZwrotu DATE,
+    DataWypozyczenia DATETIME NOT NULL,
+    DataZwrotu DATETIME NOT NULL,
 )
