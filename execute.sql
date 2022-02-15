ba7cf59b968c774853c90cb68e7d059a9e8cf32c
Julian Korgol <kontakt@juliankorgol.com>
2022-02-15 09:57:34 +0100

    2022-02-15

--- execute.sql
+++ execute.sql
@@ -1,78 +1,62 @@
+-- Trening przed sprawdzianem
+
 USE master
 GO
 
-DROP DATABASE IF EXISTS Poczta
+CREATE DATABASE Ksiegarnia
 GO
 
-CREATE DATABASE Poczta
+USE Ksiegarnia
 GO
 
-USE Poczta
-GO
-
---Zad 1
-CREATE TABLE Adresy (
-    ID INT PRIMARY KEY,
-    Ulica VARCHAR(30) NOT NULL CHECK(LEN(Ulica) >= 5),
-    NumerDomu VARCHAR(4) NOT NULL CHECK(LEN(NumerDomu) >= 1),
-    NumerLokalu VARCHAR(4),
-    KodPocztowy CHAR(6) NOT NULL CHECK(KodPocztowy LIKE '__-___')
+CREATE TABLE Ksiazki (
+    Id INT IDENTITY PRIMARY KEY,
+    Tytul VARCHAR(30) NOT NULL CHECK(LEN(Tytul) >= 3),
+    Autor VARCHAR(50) NOT NULL CHECK(LEN(Autor) >= 10),
+    DataWypozyczenia DATE,
+    LiczbaWypozyczen SMALLINT NOT NULL DEFAULT(0)
 )
 
-CREATE TABLE Paczki (
-    ID INT PRIMARY KEY,
-    Waga DECIMAL(3,1) NOT NULL CHECK(Waga >= 1),
-    DataNadania DATETIME NOT NULL,
-    Dostarczona CHAR(3) NOT NULL DEFAULT('NIE') CHECK(Dostarczona IN ('TAK', 'Nie')),
-    AdresId INT NOT NULL FOREIGN KEY REFERENCES Adresy (Id),
+ALTER TABLE Ksiazki
+ADD Stan VARCHAR(10) NOT NULL CHECK(Stan IN ('Z£Y', 'DOBRY', 'BARDZO DOBRY'))
+
+INSERT INTO Ksiazki (Tytul, Autor, DataWypozyczenia, LiczbaWypozyczen, Stan) VALUES
+('Mistrz i Ma³gorzata', 'Mikhail Bulgakov', NULL, 0, 'DOBRY'),
+('Hyperion', 'Dan Simons', '2020-05-30', 15, 'DOBRY'),
+('Lód', 'Jacek Dukaj', '2019-12-22', 2, 'DOBRY'),
+('Paragraf 22', 'Joseph Heller', '2020-10-14', 33, 'DOBRY')
+
+UPDATE Ksiazki
+SET DataWypozyczenia = GETDATE(), LiczbaWypozyczen += 1
+WHERE Tytul = 'Hyperion'
+
+
+SELECT COUNT(*) FROM Ksiazki
+WHERE YEAR(DataWypozyczenia) = YEAR(GETDATE())
+
+
+CREATE TABLE Czytelnicy (
+    IdKarty INT PRIMARY KEY,
+    Imie VARCHAR(30) NOT NULL CHECK(LEN(Imie) >= 2),
+    DrugieImie VARCHAR(30) CHECK(LEN(DrugieImie) >= 2),
+    Nazwisko VARCHAR(30) NOT NULL CHECK(LEN(Nazwisko) >= 2),
+    LiczbaWypozyczen INT NOT NULL DEFAULT(0),
 )
 
---Zad 2
-INSERT INTO Adresy (ID, Ulica, NumerDomu, NumerLokalu, KodPocztowy)
-VALUES
-(1, 'Leœna', '1', '3', '00-123'),
-(2,'Sasankowe', '10A', NULL, '10-001'),
-(3,'Chabrowa', '12', NULL, '99-777'),
-(4,'Lipowa', '44', '55', '60-006'),
-(5,'Sasankowa', '6H', '891', '00-123'),
-(6,'Cement', '2', '101', '17-120')
+INSERT INTO Czytelnicy (IdKarty, Imie, DrugieImie, Nazwisko) VALUES
+(1, 'Julian', NULL, 'Korgol')
 
-INSERT INTO Paczki
-VALUES
-(10, 1.3, 2022-01-12, 'NIE', 3),
-(20, 2.7, 2022-01-20, 'NIE', 4),
-(30, 3.0, 2022-01-19, 'NIE', 1),
-(40, 1.3, 2022-01-12, 'NIE', 3),
-(50, 5.4, 2022-01-01, 'NIE', 6),
-(60, 2.2, 2022-01-09, 'NIE', 4)
+SELECT * FROM Czytelnicy
 
---ZAD 3
-UPDATE Paczki
-SET Dostarczona = 'TAK'
-WHERE DataNadania < 2022-01-15
+CREATE TABLE TEST (
+    Test DECIMAL(4, 3),
+)
 
---ZAD 4
--- Poniewa¿ data dostarczenia nie mo¿e byæ nullem.
--- Adres ID nie mo¿e siê odwo³aæ do elementu, który nie istniejê.
--- Data jest za du¿a, nie ma 15 miesi¹ca.
--- Primary Key musi byæ unikalne.
+INSERT INTO TEST VALUES
+-- (4)
+-- (6.5)
+-- (9.9999)
+-- (57.1)
+-- (0.001)
 
---Zad 5
-DELETE Adresy
-WHERE Id IN ((SELECT Adresy.ID FROM Adresy LEFT OUTER JOIN Paczki P on Adresy.ID = P.AdresId WHERE P.id IS NULL))
-
---Zad 6
-DELETE Adresy
-WHERE ID = 6
---Poniewa¿ jest zarejestrowana na niego paczka i przez co odwo³uje siê do niego klucz obcy.
-
---Zad 7
-SELECT a.Id, a.Ulica, a.NumerDomu, a.NumerLokalu, a.KodPocztowy,
-       (SELECT COUNT(p2.ID) FROM Paczki p2 FULL OUTER JOIN Adresy A2 on A2.ID = p2.AdresId WHERE Dostarczona = 'TAK' AND A2.ID = a.ID) AS Dostarczona,
-       (SELECT COUNT(p2.ID) FROM Paczki p2 FULL OUTER JOIN Adresy A2 on A2.ID = p2.AdresId WHERE Dostarczona = 'NIE' AND A2.ID = a.ID) AS Niedostarczona
-FROM Adresy a
---Drugi sposób (Pana sposób)
-SELECT *,
-       (SELECT COUNT(*) FROM Paczki p WHERE p.AdresId = a.ID AND Dostarczona = 'TAK') LiczbaDostarczonych,
-       (SELECT COUNT(*) FROM Paczki p WHERE p.AdresId = a.ID AND Dostarczona = 'NIE') LiczbaNiedostarczonych
-FROM Adresy a
+SELECT * FROM TEST
