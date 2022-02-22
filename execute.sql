80ff2c1efd6e8a8a3246b4cf3e10dc20e66047d0
Julian Korgol <kontakt@juliankorgol.com>
2022-02-22 11:11:48 +0100

    Sprawdzian #3 - DDL i DML

--- execute.sql
+++ execute.sql
@@ -1,62 +1,65 @@
--- Trening przed sprawdzianem
-
-USE master
+use master
 GO
 
-CREATE DATABASE Ksiegarnia
+DROP DATABASE IF EXISTS Szpital
 GO
 
-USE Ksiegarnia
+CREATE DATABASE Szpital
 GO
 
-CREATE TABLE Ksiazki (
-    Id INT IDENTITY PRIMARY KEY,
-    Tytul VARCHAR(30) NOT NULL CHECK(LEN(Tytul) >= 3),
-    Autor VARCHAR(50) NOT NULL CHECK(LEN(Autor) >= 10),
-    DataWypozyczenia DATE,
-    LiczbaWypozyczen SMALLINT NOT NULL DEFAULT(0)
+use Szpital
+GO
+
+DROP TABLE IF EXISTS Lekarze
+GO
+
+CREATE TABLE Lekarze (
+    Id INT NOT NULL UNIQUE IDENTITY,
+    Imie VARCHAR(30) NOT NULL CHECK(LEN(Imie) >= 3),
+    Nazwisko VARCHAR(50) NOT NULL CHECK(LEN(Nazwisko) >= 5),
+    Specjalizacja VARCHAR(20) CHECK(Specjalizacja IN ('INTERNISTA', 'KARDIOLOG', 'CHIRURG')),
+    PoczatekDyzury TIME NOT NULL DEFAULT('7:00'),
+    KoniecDyzuru TIME NOT NULL DEFAULT('13:30')
 )
 
-ALTER TABLE Ksiazki
-ADD Stan VARCHAR(10) NOT NULL CHECK(Stan IN ('Z£Y', 'DOBRY', 'BARDZO DOBRY'))
-
-INSERT INTO Ksiazki (Tytul, Autor, DataWypozyczenia, LiczbaWypozyczen, Stan) VALUES
-('Mistrz i Ma³gorzata', 'Mikhail Bulgakov', NULL, 0, 'DOBRY'),
-('Hyperion', 'Dan Simons', '2020-05-30', 15, 'DOBRY'),
-('Lód', 'Jacek Dukaj', '2019-12-22', 2, 'DOBRY'),
-('Paragraf 22', 'Joseph Heller', '2020-10-14', 33, 'DOBRY')
-
-UPDATE Ksiazki
-SET DataWypozyczenia = GETDATE(), LiczbaWypozyczen += 1
-WHERE Tytul = 'Hyperion'
+ALTER TABLE Lekarze
+ADD LiczbaPacjentow TINYINT NOT NULL DEFAULT(0) CHECK(LEN(LiczbaPacjentow) <= 20)
 
 
-SELECT COUNT(*) FROM Ksiazki
-WHERE YEAR(DataWypozyczenia) = YEAR(GETDATE())
+INSERT INTO Lekarze (Imie, Nazwisko, Specjalizacja, PoczatekDyzury, KoniecDyzuru, LiczbaPacjentow) VALUES
+('Adam', 'Silenus', 'CHIRURG', DEFAULT, DEFAULT, 5),
+('Joanna', 'Brown', NULL, '9:00', '14:00', 13),
+('Izabela', 'Kossak', 'INTERNISTA', '7:30', '12:00', 0),
+('Zbigniew', 'Kasad', 'INTERNISTA', '8:00', '11:00', 1)
+
+SELECT * FROM Lekarze
+
+UPDATE Lekarze
+SET KoniecDyzuru = DATEADD(hour, 1, KoniecDyzuru)
+WHERE Specjalizacja = 'INTERNISTA'
+
+DELETE FROM Lekarze
+WHERE Specjalizacja IS NULL
+
+SELECT Imie, Nazwisko, PoczatekDyzury, KoniecDyzuru FROM Lekarze
+WHERE PoczatekDyzury < '12:15' AND '12:15' < KoniecDyzuru
 
 
-CREATE TABLE Czytelnicy (
-    IdKarty INT PRIMARY KEY,
-    Imie VARCHAR(30) NOT NULL CHECK(LEN(Imie) >= 2),
-    DrugieImie VARCHAR(30) CHECK(LEN(DrugieImie) >= 2),
-    Nazwisko VARCHAR(30) NOT NULL CHECK(LEN(Nazwisko) >= 2),
-    LiczbaWypozyczen INT NOT NULL DEFAULT(0),
+CREATE TABLE Pacjenci (
+    Imie VARCHAR(30) NOT NULL CHECK(LEN(Imie) >= 3),
+    DrugieImie VARCHAR(30) CHECK(LEN(DrugieImie) >= 3),
+    Nazwisko VARCHAR(50) NOT NULL CHECK(LEN(Nazwisko) >= 5),
+    PESEL CHAR(11) NOT NULL UNIQUE CHECK(LEN(PESEL) = 11),
+    Choroba VARCHAR(70) NOT NULL,
+    DataPrzybycia DATETIME NOT NULL,
+    DataWypisu DATETIME NOT NULL,
+    DodatkoweInformacje VARCHAR(300),
 )
 
-INSERT INTO Czytelnicy (IdKarty, Imie, DrugieImie, Nazwisko) VALUES
-(1, 'Julian', NULL, 'Korgol')
+INSERT INTO Pacjenci VALUES
+('Julian', NULL, 'Korgol', '01234567890', 'JaNieMogeNieJestemChory', '2021-02-22 15:00', '2021-02-22 15:01', 'DUPA')
 
-SELECT * FROM Czytelnicy
+SELECT * FROM Pacjenci
 
-CREATE TABLE TEST (
-    Test DECIMAL(4, 3),
-)
-
-INSERT INTO TEST VALUES
--- (4)
--- (6.5)
--- (9.9999)
--- (57.1)
--- (0.001)
-
-SELECT * FROM TEST
+SELECT Imie, Nazwisko FROM Lekarze
+WHERE PoczatekDyzury < '12:15' AND '12:15' < KoniecDyzuru
