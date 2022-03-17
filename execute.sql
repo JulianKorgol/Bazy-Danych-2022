ae0be8477c951a3770cbe7e65283c2b906542123
Julian Korgol <kontakt@juliankorgol.com>
2022-03-17 09:20:34 +0100

    2022-03-15

--- execute.sql
+++ execute.sql
@@ -1,65 +1,110 @@
-use master
+USE master
 GO
 
-DROP DATABASE IF EXISTS Szpital
+CREATE DATABASE Diagramy
 GO
 
-CREATE DATABASE Szpital
+USE Diagramy
 GO
 
-use Szpital
-GO
+-- Zadanie 1
+-- a
 
-DROP TABLE IF EXISTS Lekarze
-GO
-
-CREATE TABLE Lekarze (
-    Id INT NOT NULL UNIQUE IDENTITY,
+CREATE TABLE KlubSportowy (
+    ID INT NOT NULL IDENTITY PRIMARY KEY,
+    Nazwa VARCHAR(50) NOT NULL CHECK(LEN(Nazwa) >= 3),
+    LiczbaGraczy INT CHECK(LiczbaGraczy >= 2),
+    RokZalozenia SMALLINT CHECK(RokZalozenia <= YEAR(GETDATE())),
     Imie VARCHAR(30) NOT NULL CHECK(LEN(Imie) >= 3),
-    Nazwisko VARCHAR(50) NOT NULL CHECK(LEN(Nazwisko) >= 5),
-    Specjalizacja VARCHAR(20) CHECK(Specjalizacja IN ('INTERNISTA', 'KARDIOLOG', 'CHIRURG')),
-    PoczatekDyzury TIME NOT NULL DEFAULT('7:00'),
-    KoniecDyzuru TIME NOT NULL DEFAULT('13:30')
+    Nazwisko VARCHAR(30) NOT NULL CHECK(LEN(Nazwisko) >= 3),
+    Wynagrodzenie CHAR(9) NOT NULL,
 )
 
-ALTER TABLE Lekarze
-ADD LiczbaPacjentow TINYINT NOT NULL DEFAULT(0) CHECK(LEN(LiczbaPacjentow) <= 20)
+-- b
 
-
-INSERT INTO Lekarze (Imie, Nazwisko, Specjalizacja, PoczatekDyzury, KoniecDyzuru, LiczbaPacjentow) VALUES
-('Adam', 'Silenus', 'CHIRURG', DEFAULT, DEFAULT, 5),
-('Joanna', 'Brown', NULL, '9:00', '14:00', 13),
-('Izabela', 'Kossak', 'INTERNISTA', '7:30', '12:00', 0),
-('Zbigniew', 'Kasad', 'INTERNISTA', '8:00', '11:00', 1)
-
-SELECT * FROM Lekarze
-
-UPDATE Lekarze
-SET KoniecDyzuru = DATEADD(hour, 1, KoniecDyzuru)
-WHERE Specjalizacja = 'INTERNISTA'
-
-DELETE FROM Lekarze
-WHERE Specjalizacja IS NULL
-
-SELECT Imie, Nazwisko, PoczatekDyzury, KoniecDyzuru FROM Lekarze
-WHERE PoczatekDyzury < '12:15' AND '12:15' < KoniecDyzuru
-
-
-CREATE TABLE Pacjenci (
-    Imie VARCHAR(30) NOT NULL CHECK(LEN(Imie) >= 3),
-    DrugieImie VARCHAR(30) CHECK(LEN(DrugieImie) >= 3),
-    Nazwisko VARCHAR(50) NOT NULL CHECK(LEN(Nazwisko) >= 5),
-    PESEL CHAR(11) NOT NULL UNIQUE CHECK(LEN(PESEL) = 11),
-    Choroba VARCHAR(70) NOT NULL,
-    DataPrzybycia DATETIME NOT NULL,
-    DataWypisu DATETIME NOT NULL,
-    DodatkoweInformacje VARCHAR(300),
+CREATE TABLE Samochod (
+    ID INT NOT NULL IDENTITY PRIMARY KEY,
+    Marka VARCHAR(20) NOT NULL,
+    Model VARCHAR(20) NOT NULL,
+    RokProdukcji SMALLINT NOT NULL CHECK(RokProdukcji <= YEAR(GETDATE())),
+    NumerRejestracyjny VARCHAR(10) NOT NULL
 )
 
-INSERT INTO Pacjenci VALUES
-('Julian', NULL, 'Korgol', '01234567890', 'JaNieMogeNieJestemChory', '2021-02-22 15:00', '2021-02-22 15:01', 'DUPA')
+CREATE TABLE Naprawa (
+    DataNaprawy DATE NOT NULL CHECK(DataNaprawy <= GETDATE()),
+    Opis VARCHAR(max),
+    Koszt INT NOT NULL,
+    ID_Samochodu INT REFERENCES Samochod(ID)
+)
 
-SELECT * FROM Pacjenci
+-- c
 
-SELECT Imie, Nazwisko FROM Lekarze
-WHERE PoczatekDyzury < '12:15' AND '12:15' < KoniecDyzuru
+CREATE TABLE Film (
+    ID INT NOT NULL IDENTITY PRIMARY KEY,
+    Tytuł VARCHAR(50) NOT NULL,
+    RokProdukcji SMALLINT NOT NULL CHECK(RokProdukcji <= YEAR(GETDATE())),
+    Kategoria VARCHAR(20) NOT NULL,
+    Rezyser_ID INT NOT NULL REFERENCES Rezyser(ID),
+    Nazwa VARCHAR(50),
+    RokPrzyznania SMALLINT CHECK(RokPrzyznania <= YEAR(GETDATE()))
+)
+
+CREATE TABLE Rezyser (
+    ID INT NOT NULL IDENTITY PRIMARY KEY,
+    Imie VARCHAR(30) NOT NULL CHECK(LEN(Imie) >= 3),
+    Nazwisko VARCHAR(30) NOT NULL CHECK(LEN(Nazwisko) >= 3),
+)
+
+-- d
+
+CREATE TABLE Klasa (
+    ID INT NOT NULL IDENTITY PRIMARY KEY,
+    Rocznik SMALLINT NOT NULL CHECK(Rocznik <= YEAR(GETDATE())),
+    NumerKlasy SMALLINT NOT NULL,
+    Profil VARCHAR(30) NOT NULL,
+    Wychowawca_ID SMALLINT REFERENCES Wychowawca(ID)
+)
+
+CREATE TABLE Wychowawca (
+    ID INT NOT NULL IDENTITY PRIMARY KEY,
+    Imie VARCHAR(30) NOT NULL CHECK(LEN(Imie) >= 3),
+    Nazwisko VARCHAR(30) NOT NULL CHECK(LEN(Nazwisko) >= 3),
+    Przedmiot VARCHAR(30) NOT NULL
+)
+
+CREATE TABLE Wycieczka (
+    ID INT NOT NULL IDENTITY PRIMARY KEY,
+    DataWyjazdu DATETIME NOT NULL CHECK(DataWyjazdu > GETDATE()),
+    DataPowrotu DATETIME NOT NULL CHECK(DataPowrotu > Wycieczka.DataWyjazdu),
+    Cel VARCHAR(70) NOT NULL,
+    Klasa_ID INT NOT NULL REFERENCES Klasa(ID)
+)
+
+-- Zadanie 2
+
+CREATE TABLE Klient (
+    id INT IDENTITY PRIMARY KEY NOT NULL,
+    imie VARCHAR(30) NOT NULL CHECK(LEN(imie) >= 3),
+    nazwisko VARCHAR(30) NOT NULL CHECK(LEN(nazwisko) >= 3),
+    dataUrodzenia DATE NOT NULL CHECK(dataUrodzenia <= GETDATE()),
+    narodowosc VARCHAR(15) NOT NULL CHECK(narodowosc IN ('Polak', 'Niemiec', 'Francuz', 'Hiszpan') OR narodowosc IN ('Polka', 'Niemka', 'Francuska', 'Hiszpanka')),
+    typKonta VARCHAR(8) NOT NULL CHECK(typKonta IN ('firmowe', 'osobiste')),
+)
+
+CREATE TABLE RachunekBankowy (
+    numerRachunku CHAR(10) NOT NULL IDENTITY PRIMARY KEY CHECK(LEN(numerRachunku) = 10),
+    saldo BIGINT NOT NULL,
+    waluta CHAR(10) NOT NULL,
+    numerKlienta INT NOT NULL REFERENCES Klient(id),
+    dataOstatniegoLogowania DATETIME NOT NULL CHECK(dataOstatniegoLogowania <= GETDATE()),
+    NIP CHAR(10) CHECK(LEN(NIP) = 10),
+    nazwaFirmy VARCHAR(50)
+)
+
+CREATE TABLE Przelew (
+    id INT NOT NULL IDENTITY PRIMARY KEY,
+    numerKontaZrodlowego CHAR(10) NOT NULL REFERENCES RachunekBankowy(numerRachunku),
+    numerkontaDocelowego CHAR(10) NOT NULL REFERENCES RachunekBankowy(numerRachunku),
+    kwota BIGINT NOT NULL REFERENCES RachunekBankowy(saldo),
+    waluta CHAR(10) NOT NULL
+)
