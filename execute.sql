d6c16bb9e6778eb24f85404ff547c61fb0f8b3ce
Julian Korgol <kontakt@juliankorgol.com>
2022-04-05 10:34:51 +0200

    2022-04-05

--- execute.sql
+++ execute.sql
@@ -1,46 +1,15 @@
--- Zad 1
-CREATE OR ALTER PROC spWszystkieKsiazki AS
+CREATE TRIGGER trUsunAutora ON Autorzy AFTER DELETE
+    AS
     BEGIN
-        SELECT * FROM Ksiazki
-        ORDER BY DataWydania ASC
-    end
+        DECLARE @LiczbaUsuwanych INT
+        SET @LiczbaUsuwanych = (SELECT(*) FROM deleted)
 
---Zad 2
-CREATE OR ALTER PROC spKsiazkiAutora @Autor VARCHAR(50) AS
-    BEGIN
-        SELECT * FROM Ksiazki
-        WHERE Autor LIKE '%' + @Autor + '%'
-    end
-
--- Zad 3
-CREATE OR ALTER PROC spKsiazkiZKategorii @Kategoria VARCHAR(50) = 'fantastyka' AS
-    BEGIN
-        SELECT COUNT(*) FROM Ksiazki
-        WHERE Kategoria = @Kategoria
-    end
-
--- Zad 4
-CREATE OR ALTER PROC spCena @Operacja VARCHAR(10) = NULL AS
-    IF @Operacja NOT IN ('MAX', 'MIN', 'AVG')
+        IF @LiczbaUsuwanych > 1
         BEGIN
-            PRINT 'Nieznana operacja'
-            RETURN
-        END
-    IF @Operacja = 'MAX'
-        BEGIN
-            SELECT MAX(Cena) FROM Ksiazki
-            RETURN
+            DECLARE @komunikat VARCHAR(100)
+            SET @komunikat = 'Próbujesz usunąć za ' + CAST(@LiczbaUsuwanych AS VARCHAR(MAX)) + ' wierszy'
+
+                RAISERROR(@komunikat, 16,1)
+            ROLLBACK
         end
-    IF @Operacja = 'MIN'
-        BEGIN
-            SELECT MIN(Cena) FROM Ksiazki
-            RETURN
-        end
-    IF @Operacja = 'AVG'
-        BEGIN
-            SELECT AVG(Cena) FROM Ksiazki
-            RETURN
-        end
-
-
-EXEC spCena 'Nie wiem'
+    end
