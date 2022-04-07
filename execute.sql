4161dbfef5e503a02fa635c9ef5521fdb8bd5618
Julian Korgol <kontakt@juliankorgol.com>
2022-04-07 10:09:59 +0200

    2022-04-07 - wyzwalacze

--- execute.sql
+++ execute.sql
@@ -1,15 +1,74 @@
-CREATE TRIGGER trUsunAutora ON Autorzy AFTER DELETE
-    AS
+USE master
+GO
+
+USE WyzwalaczeCwiczenia12
+GO
+
+
+--Wyzwalacze
+--1
+CREATE TRIGGER trgNowyKlient
+    ON Klient AFTER INSERT AS
     BEGIN
-        DECLARE @LiczbaUsuwanych INT
-        SET @LiczbaUsuwanych = (SELECT(*) FROM deleted)
+        PRINT 'Dodano nowego klienta'
+    end
 
-        IF @LiczbaUsuwanych > 1
-        BEGIN
-            DECLARE @komunikat VARCHAR(100)
-            SET @komunikat = 'Próbujesz usunąć za ' + CAST(@LiczbaUsuwanych AS VARCHAR(MAX)) + ' wierszy'
+-- INSERT INTO Klient (Id, Imie, Nazwisko) VALUES
+--     (60, 'Julian', 'Korgol')
 
-                RAISERROR(@komunikat, 16,1)
-            ROLLBACK
-        end
+--2 - Do poprawy
+CREATE OR ALTER TRIGGER trgNoweZamowienie ON Zamowienie AFTER INSERT AS
+    BEGIN
+        DECLARE @Id VARCHAR(50)
+        SET @Id = (SELECT Id FROM inserted)
+        PRINT 'Dodano zamówienie o Id: ' + CAST(@Id AS VARCHAR(max))
+    end
+--
+-- INSERT INTO Zamowienie (KlientId, Wartosc, Status) VALUES
+--     (60, 25, 'NOWE')
+--
+-- SELECT * FROM Zamowienie
+
+
+--3
+CREATE OR ALTER TRIGGER trgUsunZamowienie ON Zamowienie AFTER DELETE AS
+    BEGIN
+        DECLARE @Status VARCHAR(30)
+        SET @Status = (SELECT Status FROM deleted)
+        IF @Status != 'ZAKONCZONE'
+            BEGIN
+                ROLLBACK TRANSACTION
+                RAISERROR('NIE MOŻNA USUNĄĆ ZAMÓWIENIA O STATUSIE INNYM NIŻ "ZAKONCZONE', 16, 2)
+            end
+    end
+--
+-- DELETE FROM Zamowienie
+-- WHERE Id = 4
+--
+-- SELECT * FROM Zamowienie
+
+--4
+CREATE OR ALTER TRIGGER trgPodwyzka ON Pracownik AFTER UPDATE AS
+    BEGIN
+        DECLARE @ZarobkiPo FLOAT
+        DECLARE @ZarobkiPrzed FLOAT
+        SET @ZarobkiPo = (SELECT Zarobki FROM inserted)
+        SET @ZarobkiPrzed = (SELECT Zarobki FROM deleted)
+        IF (@ZarobkiPo > (@ZarobkiPrzed * 1.3))
+            BEGIN
+                ROLLBACK TRANSACTION
+                RAISERROR('Zbyt wysoka podwyżka', 16, 3)
+            end
+    end
+--
+-- UPDATE Pracownik
+-- SET Zarobki = Zarobki * 1.29
+-- WHERE Id = 91
+--
+-- SELECT * FROM Pracownik
+--5
+CREATE TRIGGER trgUsuwanieTabel ON DATABASE AFTER DROP_TABLE AS
+    BEGIN
+        ROLLBACK TRANSACTION
+        RAISERROR('Usuwanie tabel zabronione', 16, 1)
     end
